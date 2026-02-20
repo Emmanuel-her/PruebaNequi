6eaf7dd29e96604799a5930afe90d97f6d1b5eac
edwin.hernandez <edwin.hernandez@dms.ms>
2026-02-20 12:33:12 -0500

    al archivo main.py le añadi una descripcion para indicar cual es el usuario y la contraseña para que cualquier persona pueda probar el endpoint

--- main.py
+++ main.py
@@ -2,17 +2,29 @@ from fastapi import FastAPI
 from app.api.endpoints import messages, websockets, auth
 from app.core.config import settings
 from app.core.database import engine, Base, SessionLocal
-from app.models import message, usuario  # noqa: asegura que ambos modelos se registren
+from app.models import message, usuario  
 
 Base.metadata.create_all(bind=engine)
 
-# Crear usuario por defecto al iniciar
 from app.services.auth_service import crear_usuario_por_defecto
 db = SessionLocal()
 crear_usuario_por_defecto(db)
 db.close()
 
-app = FastAPI(title=settings.PROJECT_NAME)
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description="""
+## API de Chat - Prueba Técnica Nequi
+
+Para probar los endpoints protegidos, primero haz login:
+
+1. Haz clic en **Authorize 🔒**
+2. Ingresa las siguientes credenciales:
+   - **Username:** `user`
+   - **Password:** `123456`
+3. Clic en **Authorize**
+"""
+)
 
 app.include_router(auth.router, prefix="/auth", tags=["Autenticación"])
 app.include_router(messages.router, prefix="/api/messages", tags=["Mensaje"])
